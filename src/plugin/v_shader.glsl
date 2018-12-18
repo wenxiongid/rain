4d9dbf96b9388da7f9991fe87601cd7a0d1167b7
胡俊文 <hujunwen@ECCMAC-C02WF70J.local>
2018-12-18 17:06:09 +0800

    fixed all img size ratio

--- src/plugin/v_shader.glsl
+++ src/plugin/v_shader.glsl
@@ -11,11 +11,13 @@ void main() {
   float ratio = u_resolution.y / u_resolution.x;
   float imgRatio = u_imgResolution.y / u_imgResolution.x;
   if(imgRatio >= ratio){
-    v_texCoord = a_Position * 0.5 + 0.5;
+    v_texCoord.x = a_Position.x * 0.5 + 0.5;
+    v_texCoord.y = a_Position.y / imgRatio * ratio * 0.5 + 0.5;
   }else{
     float w = u_resolution.x / u_resolution.y;
+    float imgW = u_imgResolution.x / u_imgResolution.y;
     v_texCoord.y = a_Position.y * 0.5 + 0.5;
-    v_texCoord.x = a_Position.x * w * 0.5 + 0.5;
+    v_texCoord.x = a_Position.x / imgW * w * 0.5 + 0.5;
   }
   gl_Position = vec4(a_Position, 0.0, 1.0);
 }
